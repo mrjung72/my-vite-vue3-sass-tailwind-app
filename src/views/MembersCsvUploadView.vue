b83db2b12ed9e469beb57c96463408081130c24d
sahara <mrjung72@gmail.com>
2025-05-24 18:27:58 +0900

    샘플파일 다운로드 링크 추가

--- src/views/MembersCsvUploadView.vue
+++ src/views/MembersCsvUploadView.vue
@@ -6,7 +6,7 @@
     <div v-if="error" class="mt-2 text-red-500">{{ error }}</div>
 		<div class="text-base-content/70 mb-10"></div>
 		<div class="text-base-content/100 mb-1">
-			<p>샘플파일을 참조하여 작성해 주세요</p>
+      <p><a href="/샘플파일/사용자정보_샘플.csv" download="사용자정보_샘플.csv" target="_blank" rel="noopener noreferrer" class="text-blue-600 hover:underline">사용자 매뉴얼 다운로드</a>을 다운로드 후 작성해 주세요</p>
 			<p>파일타입은 euc-kr로 맞춰주세요!</p>
 			<p>1번째 줄에는 타이틀을 영문으로 정의해 주세요!</p>
 		</div>
@@ -16,6 +16,8 @@
 <script setup>
 import { ref } from 'vue'
 import axios from 'axios'
+import { useRouter } from 'vue-router'
+
 
 const file = ref(null)
 const message = ref('')
@@ -41,7 +43,7 @@ const submitFile = async () => {
     error.value = ''
   } catch (err) {
     error.value = err.response?.data?.message || '업로드 실패'
-    message.value = ''
+    message.value = ''    
   }
 }
 </script>
