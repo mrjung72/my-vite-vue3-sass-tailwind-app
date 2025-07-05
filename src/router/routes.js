412475c06bf7f63963209b32fee1ca8375971bb1
sahara <mrjung72@gmail.com>
2025-07-05 16:49:54 +0900

    소스 디렉토리 정리

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,7 +1,7 @@
-import HomeView from '@/views/HomeView.vue';
+import HomeView from '@/views/system/HomeView.vue';
 import MembersView from '@/views/members/MembersView.vue';
-import MyInfoView from '@/views/MyInfoView.vue';
-import MyInfoEditView from '@/views/MyInfoEditView.vue';
+import MyInfoView from '@/views/auth/MyInfoView.vue';
+import MyInfoEditView from '@/views/auth/MyInfoEditView.vue';
 import { useAuthStore } from '../stores/auth'
 import pinia from '../stores/pinia'
 
@@ -18,20 +18,20 @@ export default [
 		path: '/preview',
 		name: 'componentsPreview',
 		// Lazy-loaded route
-		component: () => import('@/views/PreviewView.vue'),
+		component: () => import('@/views/system/PreviewView.vue'),
 		meta: { title: 'Components Preview' },
 	},
 	{
 		// 404 fallback
 		path: '/:pathMatch(.*)*',
 		name: 'notFound',
-		component: () => import('@/views/NotFoundView.vue'),
+		component: () => import('@/views/system/NotFoundView.vue'),
 		meta: { title: '404 Not Found' },
 	},
 	{ 
 		path: '/login', 
 		name: 'login',
-		component: () => import('@/views/LoginView.vue'),
+		component: () => import('@/views/auth/LoginView.vue'),
 		meta: { title: '로그인' },
 	},
 	{ 
@@ -97,61 +97,61 @@ export default [
 	{ 
 		path: '/board',
 		name: 'board',
-		component: () => import('@/views/BoardView.vue'),
+		component: () => import('@/views/board/BoardView.vue'),
 		meta: { title: '게시판', requiresLogin: true   },
 	},
 	{
 		path: '/board/write',
 		name: 'board-write',
-		component: () => import('@/views/BoardWriteView.vue'),
+		component: () => import('@/views/board/BoardWriteView.vue'),
 		meta: { title: '게시글 작성', requiresLogin: true },
 	},
 	{
 		path: '/board/:id',
 		name: 'board-detail',
-		component: () => import('@/views/BoardDetailView.vue'),
+		component: () => import('@/views/board/BoardDetailView.vue'),
 		meta: { title: '게시글 상세', requiresLogin: true },
 	},
 	{
 		path: '/board/edit/:id',
 		name: 'board-edit',
-		component: () => import('@/views/BoardEditView.vue'),
+		component: () => import('@/views/board/BoardEditView.vue'),
 		meta: { title: '게시글 수정', requiresLogin: true },
 	},
 	{
 		path: '/extract-tables',
 		name: 'extract-tables',
-		component: () => import('@/views/ExtractTableNamesView.vue'),
+		component: () => import('@/views/extract/ExtractTableNamesView.vue'),
 		meta: { title: '테이블명 추출기' },
 	},
 	{
 		path: '/extract-atwords',
 		name: 'extract-atwords',
-		component: () => import('@/views/ExtractAtWordsView.vue'),
+		component: () => import('@/views/extract/ExtractAtWordsView.vue'),
 		meta: { title: '@단어 추출기' },
 	},
 	{
 		path: '/extract-regex',
 		name: 'extract-regex',
-		component: () => import('@/views/ExtractRegexView.vue'),
+		component: () => import('@/views/extract/ExtractRegexView.vue'),
 		meta: { title: '정규식 추출기' },
 	},
 	{
 		path: '/extract-separator',
 		name: 'extract-separator',
-		component: () => import('@/views/SeparatorView.vue'),
+		component: () => import('@/views/extract/SeparatorView.vue'),
 		meta: { title: '구분자 추출기' },
 	},
 	{
 		path: '/release-history',
 		name: 'release-history',
-		component: () => import('@/views/ReleaseHistoryView.vue'),
+		component: () => import('@/views/system/ReleaseHistoryView.vue'),
 		meta: { title: '릴리즈 갱신이력' },
 	},
 	{
 		path: '/tech-stack-detail',
 		name: 'tech-stack-detail',
-		component: () => import('@/views/TechStackDetailView.vue'),
+		component: () => import('@/views/system/TechStackDetailView.vue'),
 		meta: { title: '기술스택 상세 안내' },
 	},
 ];
